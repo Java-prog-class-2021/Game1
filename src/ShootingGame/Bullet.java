b6aa1292f081d8ecf143e219897650df0c077112
sopy228 <32242022+sopy228@users.noreply.github.com>
2022-01-27 13:23:45 -0500

    Update Bullet.java

--- src/ShootingGame/Bullet.java
+++ src/ShootingGame/Bullet.java
@@ -14,6 +14,10 @@ public class Bullet {
 	
 	static double height = width;
 	
+	String filename = "laser bullet.png";
+
+	Image imgPlayerBullet = loadImage(filename);
+	
 	public Bullet () {
 		
 	}
@@ -41,6 +45,33 @@ public class Bullet {
 	void shooting() {
 		x1 += v1;
 		y1 += v2;
+	}
+	
+	Image loadImage(String filename) {
+		Image image = null;
+
+		URL imageURL = this.getClass().getResource("/" + filename);
+
+		InputStream inputStr = ShootingGame.class.getClassLoader().getResourceAsStream(filename);
+
+		if (imageURL != null) {
+			ImageIcon icon = new ImageIcon(imageURL);
+			image = icon.getImage();
+		} else {
+			JOptionPane.showMessageDialog(null, "An image failed to load: " + filename, "ERROR",
+					JOptionPane.ERROR_MESSAGE);
+		}
+
+		return image;
+	}
+
+	public void paintImg3(Graphics g) {
+
+		int imgW = imgPlayerBullet.getWidth(null);
+		int imgH = imgPlayerBullet.getHeight(null);
+
+		g.drawImage(imgPlayerBullet, (int) x1, (int) y1, (int) (x1 + width), (int) (y1 + height), 0, 0, imgW, imgH, null);
+		g.drawRect((int) x1, (int) y1, (int) width, (int) height);
 	}
 	
 	
